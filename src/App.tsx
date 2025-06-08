20589468329da35742ea8aba7dc56bea6958cec8
elreco <elreco@users.noreply.github.com>
2025-06-08 22:46:43 +0200

    Update src/App.tsx - Version 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Header } from "./components/Header";
 import { HeroSection } from "./components/HeroSection";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-background to-muted">
-      testest
+      <Header />
       <HeroSection />
     </div>
   );
